f766d59efb134e830fc3b626f74768e2c6a57cac
blek! <57577711+b1ek@users.noreply.github.com>
2020-12-24 15:08:05 +1000

    LogLib
    Added new functions to write info, warn, error, and debug issies easily

--- main.py
+++ main.py
@@ -1,27 +1,62 @@
 # LobLib by BlackLightHack
+# creeperywime@gmail.com
+# +79940067828
+# blek!#9906
 # Released under GNU license
 
 from datetime import date, datetime
 from sys import exit
+import os # нафиг не нужно
  
-now = datetime.now()
-logfilename = str(''.join((str("{}.{}.{}-{}.{}".format(now.day, now.month, now.year, now.hour, now.minute)), ".log")))
+
+now = datetime.now() # получаем текущее время, но это котреж
+logfilename = str(''.join((str("{}.{}.{}-{}.{}".format(now.day, now.month, now.year, now.hour, now.minute)), ".log"))) # Из кортежа получаю нужное время и дату, Указываю имя файлу лога
 fdh = open(logfilename, "tw", encoding="utf-8") # открыть файл
  
  
 def logTimeUpdate():
     now = datetime.now() # Получить текущее время и дату
     global logtime # глобальной
-    logtime = str("[{}:{}:{}]: ".format(now.hour, now.minute, now.second)) # Задать переменную времени
+    logtime = str("[{}:{}:{}] ".format(now.hour, now.minute, now.second)) # Задать переменную времени
  
  
 def write(obj): 
     logTimeUpdate() # Обновляем время в логе
-    tup = logtime, obj # [10:16:42]: Запущена программа(но это кортеж!)
+    tup = logtime, ": ", obj # [10:16:42]: ""(но это кортеж!)
     wrie = ''.join(tup) # переобразовываем из кортежа в string
     fdh.write(wrie) # Записать в файл
- 
-write("[INFO] Loglib is running")
 
-if SystemExit:
-    fdh.close()
+
+def info(obj):
+    logTimeUpdate()
+    tup = logtime, '[INFO]: ', obj
+    wrie = ''.join(tup) 
+    fdh.write(wrie)
+
+
+def warn(obj):
+    logTimeUpdate()
+    tup = logtime, '[WARN]: ', obj
+    wrie = ''.join(tup) 
+    fdh.write(wrie)
+
+
+def error(obj):
+    logTimeUpdate()
+    tup = logtime, '[ERROR]: ', obj
+    wrie = ''.join(tup) 
+    fdh.write(wrie)
+
+
+def debug(obj):
+    logTimeUpdate()
+    tup = logtime, '[DEBUG]: ', obj
+    wrie = ''.join(tup) 
+    fdh.write(wrie)
+ 
+
+write("[INFO]: [LogLib] Loglib is running")
+
+if SystemExit: # Если программа закрыта:
+    fdh.close() # закрыть файл
+    #  Если у вас в программе работа с файлами, используйте эту часть кода
